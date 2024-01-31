28dc25815ed472658964ecd0db82b42e54bfb615
Quinton Heald <qbh18213@uga.edu>
2024-01-31 14:58:24 -0500

    Removed e-book-app file and added basic routes

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Movies from "./Movies";
+
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <Header />
+        <Movies />
+    </div>
+  );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import User from "./components/User";
+import Navbar from "./components/Navbar";
 
-import Header from "./components/Header";
-import Movies from "./components/Movies";  // new
 
 function App() {
   return (
-    <ChakraProvider>
-      <Header />
-      <Movies />  {/* new */}
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
   )
 }
 

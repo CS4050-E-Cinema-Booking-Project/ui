5c2f4d8af72c10e065b61ec57b85b493ea747e30
Quinton Heald <qbh18213@uga.edu>
2024-02-14 17:48:12 -0500

    Fleshed out CheckoutConfirm page with styling and text

--- src/components/CheckoutConfirm.jsx
+++ src/components/CheckoutConfirm.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import '../style/CheckoutConfirm.css'
+import { Link } from "react-router-dom";
 
 const CheckoutConfirm = () => {
   return (
     <div>
-        <h1>Checkout Confirm</h1>
+        <h1 className="check-confirm-title">Enjoy the show!</h1>
+        <h3>A confirmation and order summary have been sent to your email.</h3>
+        <h3 className="order-detail-title">Order Summary:</h3>
+        <div className="order-summary">
+            <h3>Ticket Details</h3>
+            <h3>Ticket Price</h3>
+            <h3>Order Total</h3>
+        </div>
+        <Link to="/" className="return-home">
+                Continue Browsing
+        </Link>
     </div>
   );
 };

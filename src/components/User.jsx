d6bcdec5417e824745b46935b13a4ef0ae4370e2
Quinton Heald <qbh18213@uga.edu>
2024-02-15 15:16:01 -0500

    Added basic components and styling to user profile page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import '../style/User.css'
+import { Link } from "react-router-dom";
 
 const User = () => {
   return (
-    <div>
-        <h1>User</h1>
+    <div className="flex-container">
+      <div className="flex-child">
+        <div className="circle"></div>
+        <h1 className="user-name">Name</h1>
+        <h3 className="user-desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </h3>
+        <Link to="/edit-profile" className='edit-button'>Edit Profile</Link>
+      </div>
+      <div className="order-container">
+        <div className="upcoming">
+          <h1 className="order-title">Upcoming Orders</h1>
+          <div className="upcoming-box">
+            upcoming
+          </div>
+        </div>
+        <div className="past">
+          <h1 className="order-title">Past Orders</h1>
+          <div className="past-box">
+            past
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

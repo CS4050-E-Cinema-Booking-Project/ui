e67d55dd3918ae99158da28d59f2853f8dc5fe87
Quinton Heald <qbh18213@uga.edu>
2024-02-19 16:58:59 -0500

    Added additional styling to EditProfile components

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,86 +1,96 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../style/EditProfile.css";
 
 const EditProfile = () => {
   return (
     <div>
-      <div className="card-body">
-        <div class="card-title">Account Details</div>
-        <form>
-          <div>
-            <div className="name-card">
-              <label className="name-title">First name</label>
-              <input className="name-form" type="text" placeholder="Enter your first name" />
-            </div>
-            <div className="name-card">
-              <label className="name-title">Last name</label>
-              <input className="name-form" type="text" placeholder="Enter your last name" />
-            </div>
-          </div>
-          <div>
+      <div className="settings-box">
+        <div className="card-body">
+          <div class="card-title">Account Details</div>
+          <form>
             <div>
-              <label>Phone number</label>
-              <input className="settings-form" type="tel" placeholder="Enter your phone number" />
+              <div className="name-card">
+                <label className="name-title">First Name</label>
+                <input className="name-form" type="text"/>
+              </div>
+              <div className="name-card">
+                <label className="name-title">Last Name</label>
+                <input className="name-form" type="text"/>
+              </div>
             </div>
-          </div>
-          <div>
-            <label>Email address</label>
-            <input className="settings-form" type="email" placeholder="Enter your email address" />
-          </div>
-        </form>
-      </div>
-      <div className="card-body">
-        <div class="card-title">Home Address</div>
-        <form>
-          <div>
             <div>
-              <label>Address</label>
-              <input className="settings-form" type="tel" placeholder="Enter your phone number" />
+              <div className="settings-card">
+                <label className="settings-title">Phone Number</label>
+                <input className="settings-form" type="tel" placeholder="(###)-###-####" />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="name-card">
-              <label className="name-title">City</label>
-              <input className="name-form" type="text" placeholder="Enter your city" />
+            <div className="settings-card">
+              <label className="settings-title">Email Address</label>
+              <input className="settings-form" type="email"/>
             </div>
-            <div className="name-card">
-              <label className="name-title">State</label>
-              <input className="name-form" type="text" placeholder="Enter your state" />
-            </div>
-          </div>
-          <div>
-            <div className="name-card">
-              <label className="name-title">Zip</label>
-              <input className="name-form" type="text" placeholder="Enter your zip code" />
-            </div>
-          </div>
-        </form>
-      </div>
-      <div className="card-body">
-        <div class="card-title">Payment Information</div>
-        <form>
-          <div>
-            <div className="name-card">
-              <label className="name-title">First name</label>
-              <input className="name-form" type="text" placeholder="Enter your first name" />
-            </div>
-            <div className="name-card">
-              <label className="name-title">Last name</label>
-              <input className="name-form" type="text" placeholder="Enter your last name" />
-            </div>
-          </div>
-          <div>
+          </form>
+        </div>
+        <div className="card-body">
+          <div class="card-title">Home Address</div>
+          <form>
             <div>
-              <label>Phone number</label>
-              <input className="settings-form" type="tel" placeholder="Enter your phone number" />
+              <div className="settings-card">
+                <label className="settings-title">Street Address</label>
+                <input className="settings-form" type="text"/>
+              </div>
             </div>
-          </div>
-          <div>
-            <label>Email address</label>
-            <input className="settings-form" type="email" placeholder="Enter your email address" />
-          </div>
-        </form>
-        <button class="save-button" type="button">Save changes</button>
+            <div>
+              <div className="name-card">
+                <label className="name-title">City</label>
+                <input className="name-form" type="text"/>
+              </div>
+              <div className="name-card">
+                <label className="name-title">State</label>
+                <input className="name-form" type="text"/>
+              </div>
+            </div>
+            <div>
+              <div className="name-card">
+                <label className="name-title">Postal/Zip Code</label>
+                <input className="name-form" type="text"/>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="card-body">
+          <div class="card-title">Payment Information</div>
+          <form>
+            <div>
+              <div className="settings-card">
+                <label className="settings-title">Card Number</label>
+                <input className="settings-form" type="tel"/>
+              </div>
+            </div>
+            <div>
+              <div className="name-card">
+                <label className="name-title">CVC</label>
+                <input className="name-form" type="text"/>
+              </div>
+              <div className="name-card">
+                <label className="name-title">Card Expiration</label>
+                <input className="name-form" type="text" placeholder="MM/YY" />
+              </div>
+            </div>
+            <div>
+              <div className="name-card">
+                <label className="name-title">First Name</label>
+                <input className="name-form" type="text"/>
+              </div>
+              <div className="name-card">
+                <label className="name-title">Last name</label>
+                <input className="name-form" type="text"/>
+              </div>
+            </div>
+          </form>
+          <Link to="/user" className="cancel-button">Cancel</Link>
+          <button class="save-button" type="button">Save changes</button>
+        </div>
       </div>
     </div>
   );

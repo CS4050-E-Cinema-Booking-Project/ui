a7a9b9d0ecce6f3d21dfd69c7ac3b5a06cd03f64
Quinton Heald <qbh18213@uga.edu>
2024-02-20 13:29:16 -0500

    Added MovieBox component to be used on User page

--- src/components/MovieBox.jsx
+++ src/components/MovieBox.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import "../style/MovieBox.css";
 
-function MovieBox({ title, description, image, id }) {
+function MovieBox({ image, title, date, time, tickets, seats }) {
     return (
         <div className="box">
             <div className="inner">
                 <div className="left">
-                    {image && <img src={image} alt={title} id={id} />}
+                    {image && <img src={image} alt={title} className="poster"/>}
                 </div>
                 <div className="right">
                     <h2>{title}</h2>
-                    <p>{description}</p>
+                    <h3>{date}</h3>
+                    <h3>{time}</h3>
+                    <h3>{tickets}</h3>
+                    <h3>{seats}</h3>
                 </div>
             </div>
         </div>

6c64614afab04363c25a7057347f572e79b368fe
alexander-darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2024-02-21 09:37:32 -0500

    Updating Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,20 +1,33 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios'
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react";
 
 const api = axios.create({
   baseURL: 'http://localhost:8000'
 })
 
-const MoviesContext = React.createContext({
-  movies: [], fetchMovies: () => {}
-})
-
-
 export default function Movies() {
     const [movies, setMovies] = useState([]);
     const [formData, setFormData] = useState({
-      item: '',
-      description: ''
+      title: '',
+      description: '',
+      image: ''
     });
 
     const fetchMovies = async () => {
@@ -39,30 +52,122 @@ export default function Movies() {
       await api.post('/movies/',formData);
       fetchMovies();
       setFormData({
-        item: '',
-        description: ''
+        title: '',
+        description: '',
+        image: ''
       })
       
     }
+
+    function DeleteMovie({id}) {
+    
+      const deleteMovie = async () => {
+        await fetch(`http://localhost:8000/movies/${id}`, {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: { "id": id }
+        })
+        await fetchMovies()
+      }
+    
+      return (
+        <button h="1.5rem" size="sm" onClick={deleteMovie}>Delete Movie</button>
+      )
+  }
+
+  function MovieHelper({ id, fetchMovies}) {
+    return (
+      <div>
+          <UpdateMovie id={id} fetchMovies={fetchMovies}/>
+          <DeleteMovie id={id} fetchMovies={fetchMovies}/>
+      </div>
+              
+    )
+  }
+
+  function UpdateMovie({id}) {
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const [title, setTitle] = useState(id)
+    const [desc, setDesc] = useState(id)
+
+    const updateMovie = async () => {
+        await fetch(`http://localhost:8000/movies/${id}`, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ 
+            given_id: id,
+            given_title: title,
+            given_desc: desc
+           })
+        })
+        onClose()
+        await fetchMovies()
+      }
+    
+      return (
+        <>
+          <Button h="1.5rem" size="sm" onClick={onOpen}>Update Movie</Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay/>
+            <ModalContent>
+              <ModalHeader>Update Movie</ModalHeader>
+              <ModalCloseButton/>
+              <ModalBody>
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type="text"
+                    placeholder="Add a Movie"
+                    aria-label="Add a Movie"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                  />
+                <Input
+                    pr="4.5rem"
+                    type="text"
+                    placeholder="Add a Movie"
+                    aria-label="Add a Movie"
+                    value={desc}
+                    onChange={event => setDesc(event.target.value)}
+                  />
+                </InputGroup>
+              </ModalBody>
+      
+              <ModalFooter>
+                <Button h="1.5rem" size="sm" onClick={updateMovie}>Update Movie</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+  }
+
  
     return (
         <div className='container'>
           <form onSubmit={handleFormSubmit}>
             <div className="mb-3">
-              <label htmlFor="item" className='form-label'>
+              <label htmlFor="title" className='form-label'>
                 Movie Title
               </label>
               <br></br>
-              <input type='text' className='form-control' id='item' name='item' onChange={handleInputChange} value={formData.item}/>
+              <input type='text' id='title' name='title' onChange={handleInputChange} value={formData.title}/>
             </div>
             <div className="mb-3">
               <label htmlFor="description" className='form-label'>
                 Description
               </label>
               <br></br>
-              <input type='text' className='form-control' id='description' name='description' onChange={handleInputChange} value={formData.description}/>
+              <input type='text' id='description' name='description' onChange={handleInputChange} value={formData.description}/>
             </div>
-              <button type='submit' className='btn btn-primary'>
+            <div className="mb-3">
+              <label htmlFor="image" className='form-label'>
+                Image
+              </label>
+              <br></br>
+              <input type='text' id='image' name='image' onChange={handleInputChange} value={formData.image}/>
+            </div>
+              <button type='submit'>
                 Add Movie
               </button>
           </form>
@@ -72,15 +177,18 @@ export default function Movies() {
               <tr>
                 <th>Movie Title</th>
                 <th>Description</th>
+                <th>Image</th>
               </tr>
             </thead>
             <tbody>
-              {movies.map((movie) => {
+              {movies.map((movie) => (
                 <tr key={movie.id}>
-                  <td>{movie.item}</td>
+                  <td>{movie.title}</td>
                   <td>{movie.description}</td>
+                  <td><img src={movie.image} alt="" width={150} height={150}/></td>
+                  <td><MovieHelper id={movie.id} fetchMovies={fetchMovies} /> </td>
                 </tr>
-              })}
+              ))}
             </tbody>
           </table>
           

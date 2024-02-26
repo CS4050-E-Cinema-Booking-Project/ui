d3678e9f625753baf2949328153b1f3963e813a0
Quinton Heald <qbh18213@uga.edu>
2024-02-26 00:10:46 -0500

    Updated styling for login and signup + removed unnecessary files

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,50 @@
-import React from "react";
-import LoginForm from "./LoginForm";
+import React, { useState } from "react";
 import '../style/Login.css'
 
-const Login = () => {
-  return (
-    <LoginForm />
-  );
-};
 
+const Login = () => {
+
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    
+        const enteredLoginData = {
+            email: enteredEmail,
+            password: enteredPassword
+        };
+    }
+    
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    }
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    }
+
+    return(
+        <div className="login-form">
+        <form onSubmit={submitHandler}>
+          <h1 className="login-form-title">Welcome Back!</h1>
+          <label>Email</label>
+           <input
+            id="email"
+            type="text"
+            value = {enteredEmail}
+            onChange = {emailChangeHandler}
+          />
+          <label>Password</label>
+           <input
+            id="password"
+            type="password"
+            value = {enteredPassword}
+            onChange = {passwordChangeHandler}
+          />
+          <button className="login-form-button" type="submit">Login</button>
+        </form>
+      </div>
+    )
+}
 export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,151 @@
-import React from "react";
-import SignupForm from "./SignupForm";
+import React, { useState } from "react";
+import '../style/Signup.css'
 
 const Signup = () => {
-  return (
-    <SignupForm />
-  );
-};
 
+    const [enteredFirstName, setEnteredFirstName] = useState('');
+    const [enteredLastName, setEnteredLastName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPhoneNumber, setEnteredPhoneNumber] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredConfirmPassword, setConfirmPassword] = useState('');
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        document.getElementById('firstNameP').style.display = 'none';
+        document.getElementById('lastNameP').style.display = 'none';
+        document.getElementById('phoneNumberP').style.display = 'none';
+        document.getElementById('emailP').style.display = 'none';
+        document.getElementById('passwordP').style.display = 'none';
+        document.getElementById('confirmPasswordP').style.display = 'none';
+        document.getElementById('passwordNoMatch').style.display = 'none';
+
+        const enteredSignupData = {
+            firstName: enteredFirstName,
+            lastName: enteredLastName,
+            phoneNumber: enteredPhoneNumber,
+            email: enteredEmail,
+            password: enteredPassword,
+            confirmPassword: enteredConfirmPassword
+        };
+        if (enteredFirstName === undefined || enteredFirstName.length < 1) {
+            document.getElementById('firstNameP').style.display = 'block';
+        }
+        if (enteredLastName === undefined || enteredLastName.length < 1) {
+            document.getElementById('lastNameP').style.display = 'block';
+        }
+        if (enteredPhoneNumber === undefined || enteredPhoneNumber.length < 1) {
+            document.getElementById('phoneNumberP').style.display = 'block';
+        }
+        if (enteredEmail === undefined || enteredEmail.length < 1) {
+            document.getElementById('emailP').style.display = 'block';
+        }
+        if (enteredPassword === undefined || enteredPassword.length < 1) {
+            document.getElementById('passwordP').style.display = 'block';
+        }
+        if (enteredConfirmPassword === undefined || enteredConfirmPassword.length < 1) {
+            document.getElementById('confirmPasswordP').style.display = 'block';
+        }
+        if (enteredPassword != enteredConfirmPassword) {
+            document.getElementById('passwordNoMatch').style.display = 'block';
+        }
+
+        await fetch(`http://localhost:8000/users/`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(enteredSignupData)
+        })
+        
+        setEnteredFirstName('');
+        setEnteredLastName('');
+        setEnteredPhoneNumber('');
+        setEnteredEmail('');
+        setEnteredPassword('');
+        setConfirmPassword('');
+
+    }
+
+    const firstNameChangeHandler = (event) => {
+        setEnteredFirstName(event.target.value);
+    }
+
+    const lastNameChangeHandler = (event) => {
+        setEnteredLastName(event.target.value);
+    }
+    
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    }
+
+    const phoneNumberChangeHandler = (event) => {
+        setEnteredPhoneNumber(event.target.value);
+    }
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    }
+
+    const confirmPasswordChangeHandler = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    return(
+        <div className="signup-form">
+        <form onSubmit={submitHandler}>
+          <h1 className="signup-form-title">Create an Account</h1>
+          <label>First Name</label>
+          <p className="hiddenField" id="firstNameP">Field Required</p>
+          <p></p>
+           <input
+            id="first-name"
+            type="text"
+            value = {enteredFirstName}
+            onChange = {firstNameChangeHandler}
+          />
+          <label>Last Name</label>
+          <p className="hiddenField" id="lastNameP">Field Required</p>
+           <input
+            id="last-name"
+            type="text"
+            value = {enteredLastName}
+            onChange = {lastNameChangeHandler}
+          />
+          <label>Phone Number</label>
+          <p className="hiddenField" id="phoneNumberP">Field Required</p>
+           <input
+            id="phone-number"
+            type="tel"
+            value = {enteredPhoneNumber}
+            onChange = {phoneNumberChangeHandler}
+          />
+          <label>Email</label>
+          <p className="hiddenField" id="emailP">Field Required</p>
+           <input
+            id="email"
+            type="text"
+            value = {enteredEmail}
+            onChange = {emailChangeHandler}
+          />
+          <label>Password</label>
+          <p className="hiddenField" id="passwordP">Field Required</p>
+           <input
+            id="password"
+            type="password"
+            value = {enteredPassword}
+            onChange = {passwordChangeHandler}
+          />
+          <label>Confirm Password</label>
+          <p className="hiddenField" id="confirmPasswordP">Field Required</p>
+          <p className="hiddenField" id="passwordNoMatch">Passwords does not match</p>
+           <input
+            id="confirm-password"
+            type="password"
+            value = {enteredConfirmPassword}
+            onChange = {confirmPasswordChangeHandler}
+          />
+          <button className="signup-form-button" type="submit">Sign Up</button>
+        </form>
+      </div>
+    )
+}
 export default Signup;

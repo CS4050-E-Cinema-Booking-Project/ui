6a8e15f0c5fe1e239c6cfc02422d77af144509c8
Quinton Heald <qbh18213@uga.edu>
2024-02-27 22:34:02 -0500

    Added PromoBox component + Fixed UserBox styling

--- src/components/AdminPromo.jsx
+++ src/components/AdminPromo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AdminNavbar from "./AdminNavbar";
+import PromoBox from "./PromoBox";
 import '../style/Admin.css'
 
 const AdminPromo = () => {
@@ -7,7 +8,9 @@ const AdminPromo = () => {
     <div>
         <AdminNavbar />
       <div className="colRight">
-        <h1>Admin Promo</h1>
+        <PromoBox id={"1"} description={"New users receive 20% off first ticket purchase"} status={"active"}/>
+        <PromoBox id={"2"} description={"15% off orders of 3 tickets or more"} status={"active"}/>
+        <PromoBox id={"3"} description={"Free additional ticket with purchase of one ticket to Inception"} status={"inactive"}/>
       </div>
     </div>
   );

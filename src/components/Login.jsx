204f2922d6fb5b18bdbc727b4d70648acfb10b92
Quinton Heald <qbh18213@uga.edu>
2024-03-12 20:00:29 -0400

    small updates to login form box

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,50 +1,53 @@
 import React, { useState } from "react";
 import '../style/Login.css'
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-    
-        const enteredLoginData = {
-            email: enteredEmail,
-            password: enteredPassword
-        };
-    }
-    
-    const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-    const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
-    }
+    const enteredLoginData = {
+      email: enteredEmail,
+      password: enteredPassword
+    };
+  }
 
-    return(
-        <div className="login-form">
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  }
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  }
+
+  return (
+    <div className="login-page">
+      <div className="login-form">
         <form onSubmit={submitHandler}>
           <h1 className="login-form-title">Welcome Back!</h1>
-          <label>Email</label>
-           <input
+          <label className="login-input-title">Email</label>
+          <input
             id="email"
             type="text"
-            value = {enteredEmail}
-            onChange = {emailChangeHandler}
+            value={enteredEmail}
+            onChange={emailChangeHandler}
           />
-          <label>Password</label>
-           <input
+          <label className="login-input-title">Password</label>
+          <input
             id="password"
             type="password"
-            value = {enteredPassword}
-            onChange = {passwordChangeHandler}
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
           />
+          <Link to="/" className="forgot-password-button">Forgot Password?</Link>
           <button className="login-form-button" type="submit">Login</button>
         </form>
       </div>
-    )
+    </div>
+  )
 }
 export default Login;
